c6593e93e2b1338184271db1ccffe9bf6b99c698
Ben <bebuttig@microsoft.com>
2019-12-19 14:11:42 +0000

    Removed redundant unset func

--- MobCAT/MVVM/BaseShellViewModel.cs
+++ MobCAT/MVVM/BaseShellViewModel.cs
@@ -7,14 +7,14 @@ namespace Microsoft.MobCAT.MVVM
     public class BaseShellViewModel : BaseNavigationViewModel
     {
         private IRouteNavigationService _registeredService;
-        private static Func<IRouteNavigationService> _registeredServiceFunc;
+
         protected new IRouteNavigationService Navigation
         {
             get
             {
                 if (_registeredService == null)
                 {
-                    _registeredService = _registeredServiceFunc?.Invoke() ?? ServiceContainer.Resolve<IRouteNavigationService>(true);
+                    _registeredService = ServiceContainer.Resolve<IRouteNavigationService>(true);
 
                     if (_registeredService == null)
                         Logger.Warn($"No {nameof(IRouteNavigationService)} implementation has been registered.");
