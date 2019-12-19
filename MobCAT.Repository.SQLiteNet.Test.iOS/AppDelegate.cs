3295ca3c4d6e5a816f557acab077de47307d462f
Mike Parker <mikeparker@Mikes-MacBook-Pro-2017.local>
2019-12-19 16:50:40 +0000

    Ported internal repository sqlite-net test runner

---
+++ MobCAT.Repository.SQLiteNet.Test.iOS/AppDelegate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Foundation;
+using Microsoft.MobCAT.Repository.Test;
+using NUnit.Runner.Services;
+using UIKit;
+
+namespace Microsoft.MobCAT.Repository.SQLiteNet.Test.iOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication uiApplication, NSDictionary launchOptions)
+        {
+            global::Xamarin.Forms.Forms.Init();
+
+            var nunit = new NUnit.Runner.App
+            {
+                Options = new TestOptions
+                {
+                    AutoRun = true,
+                    CreateXmlResultFile = true
+                }
+            };
+
+            nunit.AddTestAssembly(typeof(Bootstrap).Assembly);
+
+            var storageFilepath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "..", "Library");
+            Bootstrap.Begin((datastoreName) => new SQLiteNetSampleRepositoryContext(Guard.NullOrWhitespace(storageFilepath), datastoreName));
+
+            LoadApplication(nunit);
+
+            return base.FinishedLaunching(uiApplication, launchOptions);
+        }
+    }
+}
