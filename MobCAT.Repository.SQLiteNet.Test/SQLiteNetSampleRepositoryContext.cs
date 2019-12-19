3b54b61e1de70e4cdece0280813ea61ccaa338d2
Mike Parker <mikeparker@Mikes-MacBook-Pro-2017.local>
2019-12-19 16:32:31 +0000

    Ported internal SQLiteNet repository test components

---
+++ MobCAT.Repository.SQLiteNet.Test/SQLiteNetSampleRepositoryContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.MobCAT.Repository.SQLiteNet.Test.Repositories;
+using Microsoft.MobCAT.Repository.Test.Abstractions;
+
+namespace Microsoft.MobCAT.Repository.SQLiteNet.Test
+{
+    public class SQLiteNetSampleRepositoryContext : BaseSQLiteNetRepositoryContext, ISampleRepositoryContext
+    {
+        ISampleRepository _sampleRepository;
+
+        public SQLiteNetSampleRepositoryContext(string folderPath, string datastoreName)
+            : base(folderPath, datastoreName) { }
+
+        public ISampleRepository SampleRepository => _sampleRepository ?? (_sampleRepository = new SQLiteNetSampleRepository(Connection));
+
+        protected override void OnResetRepositories()
+            => _sampleRepository = null;
+    }
+}
