a350d04cd2a0dc7d8b9458b8b3618e9a6694e5e6
Mike Parker <mikeparker@Mikes-MacBook-Pro-2017.local>
2019-12-19 14:39:46 +0000

    Ported refinements to internal foundational components

---
+++ MobCAT/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Microsoft.MobCAT.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> items, Action<T> action)
+        {
+            foreach (var item in items)
+                action(item);
+        }
+    }
+}
