aceaf6080a924b2027001726c565f105f08771b0
Mike Parker <mikeparker@Mikes-MacBook-Pro-2017.local>
2019-12-19 14:52:19 +0000

    Ported internal repository components

---
+++ MobCAT/Repository/Abstractions/IRepositoryContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Microsoft.MobCAT.Repository.Abstractions
+{
+    public interface IRepositoryContext
+    {
+        Task SetupAsync();
+        Task ResetAsync();
+        Task DeleteAsync();
+    }
+}
