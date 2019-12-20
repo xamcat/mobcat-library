4e463541bd7ec253b0f75a6262c21f8be92a2313
Mike Parker <mikeparker@mikes-mbp-2017.home>
2019-12-20 11:15:57 +0000

    Ported EFCore test runners

---
+++ MobCAT.Repository.EntityFrameworkCore.Test/EFCoreSampleRepositoryContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.MobCAT.Repository.EntityFrameworkCore.Test.Repositories;
+using Microsoft.MobCAT.Repository.Test.Abstractions;
+
+namespace Microsoft.MobCAT.Repository.EntityFrameworkCore.Test
+{
+    public class EFCoreSampleRepositoryContext : BaseEFCoreRepositoryContext, ISampleRepositoryContext
+    {
+        ISampleRepository _sampleRepository;
+
+        public EFCoreSampleRepositoryContext(string folderPath, string datastoreName)
+            : base(folderPath, datastoreName) { }
+
+        public ISampleRepository SampleRepository => _sampleRepository ?? (_sampleRepository = new EFCoreSampleRepository(Connection));
+
+        protected override void OnResetRepositories()
+            => _sampleRepository = null;
+    }
+}
---
+++ MobCAT.Repository.EntityFrameworkCore.Test/Models/EFCoreSampleModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.MobCAT.Repository.EntityFrameworkCore.Test.Models
+{
+    public class EFCoreSampleModel : BaseEFCoreModel
+    {
+		public string SampleString { get; set; }
+		public int SampleInt { get; set; }
+		public long TimestampTicks { get; set; }
+	}
+}
